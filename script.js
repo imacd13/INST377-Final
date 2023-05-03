2860648a56270ea93378070ab7b24ccc34436bcb
Isabella Macdonald <imacd13@terpmail.umd.edu>
2023-05-02 23:39:15 -0400

    HTML fixes

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ async function mainEvent() {
     const loadDataButton = document.querySelector("#data_load");
 
     const loadAnimation = document.querySelector(#data_load_animation);
+    loadAnimation.style.display = "none";
 }
